803570e01005511fdd5543b3f2a2a012057e6f02
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-25 21:22:03 -0700

    Add files via upload
    Update files

---
+++ part3/json.js
@@ -0,0 +1,22 @@
+{"management":[
+    {
+        "name": "Stew Alot",
+        "title": "VP of Fun",
+        "hobbies": "Stew likes to prance around in open fields of daisies and go cow tipping."
+    },
+    {
+        "name": "Sandra Dee",
+        "title": "Matron of Ladies",
+        "hobbies": "Sandra, Sandy for short, likes to wear pink jackets and black leather and sing about the guy she's dating."
+    },
+    {
+        "name": "Billy Bob Jean",
+        "title": "Master of Men",
+        "hobbies": "Billy, B for short, really likes Sandy, but thinks she's out of his league. He plays in a office softball team that he created."
+    },
+    {
+        "name": "Amanda Pinch",
+        "title": "The Iron Fist of HR",
+        "hobbies": "Amanda, true to her name loves to pinch people when she's excited, but does her best to refrain to be a good example for the company."
+    }
+]}
